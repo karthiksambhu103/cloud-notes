0e69e81c6a7804327c8d59b9c5b18d15f6a81f34
circuitkraft <circuitkraft@gmail.com>
2025-02-20 10:27:24 +0530

    server updated

--- server/db.js
+++ server/db.js
@@ -1,11 +1,21 @@
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb+srv://kartik:sambhu@cluster0.vaq2nh0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" 
+const mongoURI = "mongodb+srv://kartik:sambhu@cluster0.vaq2nh0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-const connectToMongo = ()=>{
-    mongoose.connect(mongoURI, ()=>{
+const connectToMongo = async () => {
+    try {
+        mongoose.set('strictQuery', false);
+        
+        await mongoose.connect(mongoURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
         console.log("Connected to Mongo Successfully");
-    })
-}
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+        process.exit(1); // Exit the app if MongoDB connection fails
+    }
+};
 
 module.exports = connectToMongo;
